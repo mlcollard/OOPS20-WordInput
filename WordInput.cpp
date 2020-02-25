d952b5e19f079fe7d251707e54608813001ed061
Michael L. Collard, Ph.D <collard@uakron.edu>
2020-02-25 13:58:14 -0500

    Improve word detection

--- WordInput.cpp
+++ WordInput.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "WordInput.hpp"
+#include <ctype.h>
 
 // read all the words from a file and apply process() to them
 void wordRead(std::istream& input,
@@ -12,6 +13,10 @@ void wordRead(std::istream& input,
 
     std::string word;
     while (input >> word) {
+
+        // words must begin with an alphabetic character
+        if (!isalpha(word[0]))
+            continue;
 
         process(word);
     }
